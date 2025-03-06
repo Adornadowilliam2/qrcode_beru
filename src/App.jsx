9a2364543d8be0b433bc5d5c11ac9f2d988cabb5
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-03-06 19:43:22 +0800

    Add

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useRef, useState } from 'react';
+import EasyQRCode from 'easyqrcodejs';
+import { toast, ToastContainer } from 'react-toastify';
+
+const App = () => {
+  const [data, setData] = useState('');
+  const qrCodeRef = useRef(null);
+
+  const handleInputChange = (event) => {
+    setData(event.target.value);
+  };
+
+  const downloadQRCode = () => {
+    const canvas = qrCodeRef.current.querySelector('canvas');
+    if (canvas) {
+      const dataUrl = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.href = dataUrl;
+      link.download = 'qrcode.png';
+      link.click();
+      toast.success('Download successful!');
+    }
+  };
+
+  
+  const isValidURL = (url) => {
+    try {
+      new URL(url); 
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    if (data) {
+      
+      if (!isValidURL(data)) {
+        qrCodeRef.current.innerHTML = ''; 
+        toast.error('Please enter a valid URL.');
+        return; 
+      }
+
+      const options = {
+        text: data,
+        width: 256, 
+        height: 256, 
+        colorDark: '#000000', 
+        colorLight: '#ffffff', 
+        correctLevel: EasyQRCode.CorrectLevel.L, 
+      };
+
+      
+      new EasyQRCode(qrCodeRef.current, options);
+    } else {
+      qrCodeRef.current.innerHTML = ''; 
+    }
+  }, [data]);
+
+  return (
+    <main className="flex justify-center items-center h-full bg-black text-white">
+      <div>
+        <ToastContainer position="bottom-right" />
+        <h1>Generate QR Code</h1>
+
+        {/* Input field for the URL or any data */}
+        <input
+          type="text"
+          placeholder="Enter a valid URL to encode in QR code"
+          value={data}
+          onChange={handleInputChange}
+          className="p-2 my-5 w-78"
+        />
+
+        {/* QR Code will be rendered here */}
+        <div ref={qrCodeRef} className="flex justify-center mb-2"></div>
+
+     
+        <button
+          onClick={downloadQRCode}
+          disabled={!data ? !data : !isValidURL(data)}
+     
+          className='p-10px mt-20px block m-auto'
+        >
+          Download QR Code
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default App;
